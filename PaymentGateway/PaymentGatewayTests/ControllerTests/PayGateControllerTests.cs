03a315b759e174e1722d2219a6d57d32692f6524
Dinesh VS <dinu.vs@gmail.com>
2021-11-02 06:11:14 +0000

    add unit tests project and unit tests initial

---
+++ PaymentGateway/PaymentGatewayTests/ControllerTests/PayGateControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaymentGatewayTests.ControllerTests
+{
+    [TestClass]
+    public class PayGateControllerTests
+    {
+        [TestMethod]
+        public void Processpayment_ValidInput_ReturnsSuccess()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Processpayment_InvalidInput_ReturnNotSuccess()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Retrievepayment_validId_ReturnsNonZeroPaymentDetails()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Retrievepayment_validId_ReturnsPaymentDetails()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Retrievepayment_InvalidId_ReturnsNullOrZeroPaymentDetails()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Retrievepayment_InvalidId_ReturnsNoDataMessage()
+        {
+            Assert.Fail();
+        }
+    }
+}
